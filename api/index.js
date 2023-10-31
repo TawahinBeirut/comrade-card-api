c1f839080100469fecf4f08dc9984e224dba2121
tawahinBeirut <gheribij59@gmail.com>
2023-10-31 10:50:11 +0100

    Restructuration for vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { ApolloServer} = require('apollo-server-express');
-const schema = require('./src/schema');
+const schema = require('../src/schema');
+const { v4 } = require('uuid');
 
 
 const corsOptions = {
@@ -18,6 +19,20 @@ app.get('/',(req,res) => {
     res.json("Backend Fonctionnel")
 })
 
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+
 const startServer = async () => {
     await server.start();
 };
@@ -32,3 +47,5 @@ startServer()
     .finally(() => {app.listen(PORT,() => {
     console.log("Listening ... ");
 })})
+
+module.exports = app;
